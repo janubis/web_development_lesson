dc432b12e77636331b04ea5ed87d711e38f7badb
Javkhlan <r.javkhlan@gmail.com>
2021-11-22 11:06:29 +0800

    link done

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -4,9 +4,12 @@ import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
 
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Switch>
         <Route path='/' exact={true}>
           <AllMeetupsPage/>
